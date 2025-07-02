d9039daf8ea7a5d32e4b70211927af42e4961083
nine19een <nine19eenowo@gmail.com>
2025-07-02 08:20:58 +0800

    Creat Luogu-B2002-C++

---
+++ Luogu-B2002-C++.cpp
@@ -0,0 +1,11 @@
+// Problem:  Luogu
+// Link:     https://www.luogu.com.cn/problem/B2002
+// Author:   nine19een
+// Date:     2025-07-02
+
+#include<iostream>
+using namespace std;
+int main() {
+	cout<<"Hello,World!"<<endl;
+	return 0;
+}
