a27c80cc72fa1a176ff520967895511306c2f793
nine19een <nine19eenowo@gmail.com>
2025-07-02 10:15:57 +0800

    Add solution for Luogu P1001

---
+++ Luogu-P1001-C++.cpp
@@ -0,0 +1,12 @@
+// Problem:  P1001
+// Link:     https://www.luogu.com.cn/problem/P1001
+// Author:   nine19een
+// Date:     2025-07-02
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int a , b;
+	cin >> a >> b;
+	cout << a + b;
+}
