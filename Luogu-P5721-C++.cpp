f71f855a71a7834d456b307dcd5f7740d6daf3cf
nine19een <nine19eenowo@gmail.com>
2025-07-02 22:13:08 +0800

    Creat Luogu-P5721-C++

---
+++ Luogu-P5721-C++.cpp
@@ -0,0 +1,25 @@
+// Problem:  Luogu
+// Link:     https://www.luogu.com.cn/problem/P5721
+// Author:   nine19een
+// Date:     2025-07-02
+
+#include<bits/stdc++.h>
+using namespace std;
+int arr[100],n,m;
+int main(){
+	cin >> n;
+	m = (1 + n) * n / 2;
+	int t = 0;
+	for( int i = 1 ; i <= m ; i++){
+		arr[i] = i;
+	} 
+	for( int j = n ; j > 0 ; j--){
+		for( int i = 1 ; i <= j ; i++){
+			t++;
+			if(t < 10){
+				cout << "0" << t;
+			}else cout << t;
+		}cout << "\n";
+	}
+	return 0;
+}
