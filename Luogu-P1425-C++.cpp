b0e2740fac65406403f71d864f9ac272b278363a
nine19een <nine19eenowo@gmail.com>
2025-07-02 11:11:14 +0800

    Creat Luogu-P1425-C++

---
+++ Luogu-P1425-C++.cpp
@@ -0,0 +1,20 @@
+// Problem:  Luogu
+// Link:     https://www.luogu.com.cn/problem/P1425
+// Author:   nine19een
+// Date:     2025-07-02
+
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+	int a;
+	int b;
+	int c;
+	int d;
+	cin >> a >> b >> c >> d;
+	if ( d >= b ){
+		cout << c - a << " " << d - b;
+	} else{
+		cout << c - a - 1 << " " << d + 60 - b;
+	}
+	return 0;
+}
