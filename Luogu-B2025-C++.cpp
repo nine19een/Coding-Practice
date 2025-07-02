fd7b40017bbe796710f231214af213c588ab65e6
nine19een <nine19eenowo@gmail.com>
2025-07-02 08:36:43 +0800

    Creat Luogu-B2025-C++

---
+++ Luogu-B2025-C++.cpp
@@ -0,0 +1,15 @@
+// Problem:  Luogu
+// Link:     https://www.luogu.com.cn/problem/B2025
+// Author:   nine19een
+// Date:     2025-07-02
+
+#include<bits/stdc++.h>
+using namespace  std;
+int main() {
+	cout<<"  *"<<endl;
+	cout<<" ***"<<endl;
+	cout<<"*****"<<endl;
+	cout<<" ***"<<endl;
+	cout<<"  *"<<endl;
+	return 0;
+} 
