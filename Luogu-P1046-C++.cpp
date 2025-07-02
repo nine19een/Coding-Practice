ea1f8a3fac81c73fe83974c14969f0677a335aa5
nine19een <nine19eenowo@gmail.com>
2025-07-02 20:38:16 +0800

    Creat Luogu-P1046-C++

---
+++ Luogu-P1046-C++.cpp
@@ -0,0 +1,18 @@
+// Problem:  Luogu P1046 [NOIP 2005 ÆÕ¼°×é] ÌÕÌÕÕªÆ»¹û
+// Link:     https://www.luogu.com.cn/problem/P1046
+// Author:   nine19een
+// Date:     2025-07-02
+
+#include<bits/stdc++.h>
+using namespace std;
+int a[10],h,n;
+int main(){
+	for(int i = 1 ; i <= 10 ; i++) cin >> a[i];
+	cin >> h;
+	h += 30;
+	int n = 0;
+	for(int i = 1 ; i <= 10 ; i++){
+		if(h >= a[i]) n++;
+	}cout << n;
+	return 0;
+}
