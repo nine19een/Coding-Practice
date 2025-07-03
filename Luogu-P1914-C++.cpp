abef67f978dc8c36293125057c5e8bb41498245c
nine19een <nine19eenowo@gmail.com>
2025-07-03 21:14:52 +0800

    Creat Luogu-P1914-C++

---
+++ Luogu-P1914-C++.cpp
@@ -0,0 +1,24 @@
+// Problem:  Luogu
+// Link:     https://www.luogu.com.cn/problem/P1914
+// Author:   nine19een
+// Date:     2025-07-03
+
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+char m;
+string str;
+int main()
+{
+	cin >> n;
+	cin >> str;
+	for( int i = 0 ; i < str.length() ; i++)
+	{
+		m = str[i] ;
+		if( m + n <= 122)
+			cout << char ( m + n );
+		else
+			cout << char ( m + n - 26);
+	}
+	return 0;
+}
