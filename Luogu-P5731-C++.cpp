114a0b07f745fe1b06fd82243037d195cdd1dbdc
nine19een <nine19eenowo@gmail.com>
2025-07-03 10:58:48 +0800

    Creat Luogu-P5731-C++

---
+++ Luogu-P5731-C++.cpp
@@ -0,0 +1,35 @@
+// Problem:  Luogu
+// Link:     https://www.luogu.com.cn/problem/P5731
+// Author:   nine19een
+// Date:     2025-07-03
+
+#include<bits/stdc++.h>
+using namespace std;
+int tot=1,n;
+int a[105][105];
+int main ()
+{
+	cin >> n;
+	int x=1 , y=1;
+	a[1][1]=tot++;
+	while(tot <= n*n)
+	{
+		while(y+1 <= n && a[x][y+1] == 0)
+			a[x][++y] = tot++;
+		while(x+1 <= n && a[x+1][y] == 0)
+			a[++x][y] = tot++;
+		while(y-1 >= 1 && a[x][y-1] == 0)
+			a[x][--y] = tot++;
+		while(x-1 >= 1 && a[x-1][y] == 0)
+			a[--x][y] = tot++;
+	}
+	for( int i = 1 ; i <= n ; i++)
+	{
+		for( int j = 1 ; j <= n ; j++)
+		{
+			printf("%3d" , a[i][j]);
+		}
+		cout << endl;
+	}
+	return 0;
+}
