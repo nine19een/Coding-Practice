999b2447698b727352b045acbaa4e82fe2b3a02d
nine19een <nine19eenowo@gmail.com>
2025-07-03 16:20:19 +0800

    Creat Luogu-B2106-C++

---
+++ Luogu-B2106-C++.cpp
@@ -0,0 +1,29 @@
+// Problem:  Luogu
+// Link:     https://www.luogu.com.cn/problem/B2106
+// Author:   nine19een
+// Date:     2025-07-03
+
+#include<bits/stdc++.h>
+using namespace std;
+int n,m;
+int a[105][105];
+int main()
+{
+	cin >> n >> m;
+	for( int i = 1 ; i <= n ; i++)
+	{
+		for( int j = 1 ; j <= m ; j++)
+		{
+			cin >> a[i][j];
+		}
+	}
+	for( int i = 1 ; i <= m ; i++)
+	{
+		for( int j = 1 ; j <= n ; j++)
+		{
+			cout << a[j][i] << " " ;
+		}
+		cout << endl;
+	}
+	return 0;
+}
