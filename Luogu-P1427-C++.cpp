6230aa73a98fbf9f16209c9cae175120b9de2ae0
nine19een <nine19eenowo@gmail.com>
2025-07-03 22:44:33 +0800

    Creat Luogu-P1427-C++

---
+++ Luogu-P1427-C++.cpp
@@ -0,0 +1,24 @@
+// Problem:  Luogu
+// Link:     https://www.luogu.com.cn/problem/P1427
+// Author:   nine19een
+// Date:     2025-07-03
+
+#include<bits/stdc++.h>
+using namespace std;
+int n,c=0;
+int a[105];
+int main()
+{
+	while( cin >> n)
+	{
+		if( n == 0)
+			break;
+		c++;
+		a[c] = n;
+	}
+	for( int i = c ; i >= 1 ; i--)
+	{
+		cout << a[i] << " ";
+	}
+	return 0;
+}
