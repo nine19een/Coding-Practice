4644a26ba54f961ee144bae352db3687b884695d
nine19een <nine19eenowo@gmail.com>
2025-07-04 10:04:00 +0800

    Creat Luogu-P5015-C++

---
+++ Luogu-P5015-C++.cpp
@@ -0,0 +1,20 @@
+// Problem:  Luogu
+// Link:     https://www.luogu.com.cn/problem/P5015
+// Author:   nine19een
+// Date:     2025-07-04
+
+#include<bits/stdc++.h>
+using namespace std;
+string str;
+int t = 0;
+int main()
+{
+	getline( cin , str);
+	for( int i = 0 ; i <str.length() ; i++)
+	{
+		if( str[i] != 32 && str[i] != 10 )
+			t++;
+	}
+	cout << t;
+	return 0;
+}
