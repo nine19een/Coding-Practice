d9c1ef190dd93fd687a7d6b9806e4fabc88e3a3e
nine19een <nine19eenowo@gmail.com>
2025-07-04 10:57:19 +0800

    Creat Luogu-P1321-C++

---
+++ Luogu-P1321-C++.cpp
@@ -0,0 +1,50 @@
+// Problem:  Luogu
+// Link:     https://www.luogu.com.cn/problem/P1321
+// Author:   nine19een
+// Date:     2025-07-04
+
+#include<bits/stdc++.h>
+using namespace std;
+string str;
+int boy = 0 , girl = 0;
+int main()
+{
+	cin >> str;
+	for( int i = 0 ; i < str.length() ; i++)
+	{
+		if(str[i] == 'b' && str[i+1] == 'o' && str[i+2] == 'y')
+		{
+			boy++;
+			i += 2;
+		}
+		else if((str[i] == 'b' && str[i+1] == 'o' && str[i+2] != 'y') || (str[i] == 'o' && str[i+1] == 'y'))
+		{
+			boy++;
+			i += 1;
+		}
+		else if((str[i] == 'b' && str[i+1] != 'o') ||(str[i] == 'o' && str[i+1] != 'y') ||str[i] == 'y')
+		{
+			boy++;
+		}
+		else if(str[i] == 'g' && str[i+1] == 'i' && str[i+2] == 'r' && str[i+3] == 'l')	
+		{
+			girl++;
+			i += 3;
+		}			
+		else if((str[i] == 'g' && str[i+1] == 'i' && str[i+2] == 'r' && str[i+3] != 'l') || (str[i] == 'i' && str[i+1] == 'r' && str[i+2] == 'l'))
+		{
+			girl++;
+			i += 2;
+		}	
+		else if((str[i] == 'g' && str[i+1] == 'i' && str[i+2] != 'r') || (str[i] == 'i' && str[i+1] == 'r' && str[i+2] != 'l') || (str[i] == 'r' && str[i+1] == 'l'))
+		{
+			girl++;
+			i += 1;
+		}		
+		else if((str[i] == 'g' && str[i+1] != 'i') ||(str[i] == 'i' && str[i+1] != 'r') || (str[i] == 'r' && str[i+1] != 'l') || str[i] == 'l')
+		{
+			girl++;
+		}	
+	}
+	cout << boy << "\n" << girl;
+}
