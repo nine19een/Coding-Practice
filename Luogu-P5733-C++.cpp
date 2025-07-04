445c0e7d259c06f75fc388fcd32447f47d02e3b0
nine19een <nine19eenowo@gmail.com>
2025-07-04 11:19:24 +0800

    Creat Luogu-P5733-C++

---
+++ Luogu-P5733-C++.cpp
@@ -0,0 +1,22 @@
+// Problem:  Luogu
+// Link:     https://www.luogu.com.cn/problem/P5733
+// Author:   nine19een
+// Date:     2025-07-04
+
+#include<bits/stdc++.h>
+using namespace std;
+string str;
+char c;
+int main()
+{
+	cin >> str;
+	for( int i = 0 ; i < str.length() ; i++)
+		if( str[i] < 123 && str[i] > 96) 
+		{
+			c = str[i] - 32;
+			cout << c;
+		}
+		else
+			cout << str[i];	
+		return 0;
+}
