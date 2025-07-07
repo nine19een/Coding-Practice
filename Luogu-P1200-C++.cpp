807116058f49bf8b9609d8322ec87612fcaf4caf
nine19een <nine19eenowo@gmail.com>
2025-07-07 17:13:56 +0800

    Creat Luogu-P1200-C++

---
+++ Luogu-P1200-C++.cpp
@@ -0,0 +1,34 @@
+// Problem:  Luogu
+// Link:     https://www.luogu.com.cn/problem/P1200
+// Author:   nine19een
+// Date:     2025-07-07
+
+#include <bits/stdc++.h>
+using namespace std;
+char group[10], comet[10];
+long long count_c = 1, count_g = 1;
+int main()
+{
+	cin >> comet >> group;
+	int len_c = strlen(comet);
+	int len_g = strlen(group);
+	for(int i = 0; i < len_c; i++)
+	{
+		if(i == 0)	
+			count_c	= comet[i] - 64;
+		else
+			count_c *= comet[i] - 64;
+	}
+	for(int i = 0; i < len_g; i++)
+	{
+		if(i == 0)	
+			count_g	= group[i] - 64;
+		else
+			count_g *= group[i] - 64;
+	}
+	if(count_c % 47 == count_g % 47)	
+		cout << "GO";
+	else 
+		cout << "STAY";
+	return 0;
+}
