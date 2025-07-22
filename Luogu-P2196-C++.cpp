53135489262ed08483f1fe005b3349a89cca01c4
nine19een <nine19eenowo@gmail.com>
2025-07-23 03:56:26 +0800

    Creat Luogu-P2196-C++

---
+++ Luogu-P2196-C++.cpp
@@ -0,0 +1,55 @@
+// Problem:  Luogu
+// Link:     https://www.luogu.com.cn/problem/P2196
+// Author:   nine19een
+// Date:     2025-07-23
+
+#include<bits/stdc++.h>
+using namespace std;
+int n, maxdpj, idx, maxdp, ans;
+int boom[25], connect[25][25], dp[25] = {0}, front[25] = {0};
+
+void INVERT(int i){
+	if(front[i] != 0){
+		INVERT(front[i]);
+	}
+	cout << i << " ";
+	return;
+}
+
+void DIG(){
+	dp[1] = boom[1];
+	maxdp = boom[1];
+	ans = 1;
+	for(int i = 2; i <= n; i++){
+		maxdpj = 0, idx = 0;
+		for(int j = 1; j < i; j++){
+			if(connect[j][i] == 1 && dp[j] > maxdpj){
+				maxdpj = dp[j];
+				idx = j;
+			}
+		}
+		dp[i] = boom[i] + maxdpj;
+		front[i] = idx;
+		if(dp[i] > maxdp){
+			maxdp = dp[i];
+			ans = i;
+		}
+	}
+	return;
+}
+
+int main(){
+	cin >> n;
+	for(int i = 1; i <= n; i++){
+		cin >> boom[i];
+	}
+	for(int i = 1; i < n; i++){
+		for(int j = i + 1; j <= n; j++){
+			cin >> connect[i][j];
+		}
+	}
+	DIG();
+	INVERT(ans);
+	cout << endl << maxdp;
+	return 0;
+} 
