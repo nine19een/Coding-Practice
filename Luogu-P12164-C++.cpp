52df98a47a8f7a12b899605930514839645335ef
nine19een <nine19eenowo@gmail.com>
2025-08-23 23:55:54 +0800

    Creat Luogu-P12164-C++

---
+++ Luogu-P12164-C++.cpp
@@ -0,0 +1,32 @@
+// Problem:  Luogu
+// Link:     https://www.luogu.com.cn/problem/P12164
+// Author:   nine19een
+// Date:     2025-08-23
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int h, w;
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	cin >> h >> w;
+	int a[] = {2, 0, 2, 5};
+	int cnt, start = 0;
+	for(int i = 1; i <= h; ++i){
+		cnt = start;
+		for(int j = 1; j <= w; ++j){
+			cout << a[cnt++];
+			if(cnt == 4){
+				cnt = 0;
+			}
+		}
+		cout << '\n';
+		start++;
+		if(start == 4){
+			start = 0;
+		}
+	}
+	return 0;
+}
