80300ebcefb0cea4bcf14e325d438515013a286e
nine19een <nine19eenowo@gmail.com>
2025-09-12 23:03:56 +0800

    Creat Luogu-P8649-C++

---
+++ Luogu-P8649-C++.cpp
@@ -0,0 +1,33 @@
+// Problem:  Luogu
+// Link:     https://www.luogu.com.cn/problem/P8649
+// Author:   nine19een
+// Date:     2025-09-12
+
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int const maxn = 1e5 + 5;
+int n, k;
+ll a[maxn], pre[maxn], cnt[maxn], ans;
+
+ll CNT(ll x){
+	return x * (x - 1) / 2;
+}
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	cin >> n >> k;
+	cnt[0]++;
+	for(int i = 1; i <= n; ++i){
+		cin >> a[i];
+		pre[i] = pre[i - 1] + a[i];
+		cnt[pre[i] % k]++;
+	}
+	for(int i = 0; i < k; ++i){
+		ans += CNT(cnt[i]);
+	}
+	cout << ans;
+	return 0;
+}
