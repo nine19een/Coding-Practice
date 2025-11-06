d1e0c25a1ed46e1efd5e20f4f96f35843bb4055c
nine19een <nine19eenowo@gmail.com>
2025-11-06 14:24:41 +0800

    Creat Luogu-P1048-C

---
+++ Luogu-P1048-C.cpp
@@ -0,0 +1,33 @@
+// Problem:  Luogu P1048 [NOIP 2005 普及组] 采药
+// Link:     https://www.luogu.com.cn/problem/P1048
+// Author:   nine19een
+// Date:     2025-11-06
+
+#include <stdio.h>
+
+int dp[1005], T, M, time[105], val[105];
+
+int max(int x, int y){
+	return x > y ? x : y;
+}
+
+int DP(){
+	for(int i = 1; i <= M; ++i){
+		for(int j = T; j >= 1; --j){
+			if(time[i] > j){
+				break;
+			}
+			dp[j] = max(dp[j], dp[j - time[i]] + val[i]);
+		}
+	}
+	return dp[T];
+}
+
+int main(){
+	scanf("%d %d", &T, &M);
+	for(int i = 1; i <= M; ++i){
+		scanf(" %d %d", &time[i], &val[i]);
+	}
+	printf("%d", DP());
+	return 0;
+}
