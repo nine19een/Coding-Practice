359cbdd1fc59b88458087f08a96ddb28637a66b2
nine19een <nine19eenowo@gmail.com>
2025-11-11 18:27:08 +0800

    Update Luogu-B2118-C++.cpp

--- Luogu-B2118-C++.cpp
+++ Luogu-B2118-C++.cpp
@@ -8,32 +8,19 @@ using namespace std;
 
 string s1, s2;
 
-void match(int len1, int len2, string &s1, string &s2) {
-    for (int i = 0; i < len2; i++) {
-        if (s2[i] == s1[0]) {
-            bool y = true;
-            for (int j = 0; j < len1; j++) {
-                if (s1[j] != s2[i + j]) {
-                    y = false;
-                    break;
-                }
-            }
-            if (y) {
-                cout << s1 << " is substring of " << s2;
-                return;
-            }
-        }
+void match(string &a, string &b) {
+    if (b.find(a) != string::npos) {
+        cout << a << " is substring of " << b;
+    } else {
+        cout << "No substring";
     }
-    cout << "No substring";
 }
 
 void check() {
-    int len1 = s1.length(), len2 = s2.length();
-    if (len1 <= len2) {
-        match(len1, len2, s1, s2);
-    }
-    else {
-        match(len2, len1, s2, s1);
+    if (s1.length() <= s2.length()) {
+        match(s1, s2);
+    } else {
+        match(s2, s1);
     }
 }
 
