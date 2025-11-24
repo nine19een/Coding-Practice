692ad0fe3eb1ee58518b2f269b007775ce6ff4ea
nine19een <nine19eenowo@gmail.com>
2025-11-24 14:31:25 +0800

    update P1147

--- Luogu-P1147-C++.cpp
+++ Luogu-P1147-C++.cpp
@@ -8,30 +8,19 @@ using namespace std;
 using ll = long long;
 int const maxn = 2e6 + 5;
 
-struct Prefix {
-    ll val, idx;
-};
-
-ll n, a[maxn];
-vector<Prefix> prefix;
+ll n, a[maxn], prefix[maxn];
 
 int main() {
-    prefix.reserve(maxn);
     cin >> n;
-    prefix.push_back({0, 0});
     for (int i = 1; i <= n; i++) {
         a[i] = i;
-        Prefix num = {a[i] + prefix[i - 1].val, i};
-        prefix.push_back(num);
+        prefix[i] = a[i] + prefix[i - 1];
     }
-    for (auto it = prefix.begin(); it != prev(prefix.end()); it++) {
-        ll aim_num = n + it->val;
-        Prefix comp = {aim_num, 0};
-        auto It = lower_bound(it, prefix.end(), comp, [](Prefix const &a, Prefix const &b) {
-            return a.val < b.val;
-        });
-        if (It != prefix.end() && It->val == aim_num && It->idx != n) {
-            cout << (it->idx + 1) << " " << (It->idx) << endl;
+    for (int i = 1; i < n; ++i) {
+        ll aim_num = n + prefix[i - 1];
+        int r_idx = lower_bound(prefix + i, prefix + n + 1, aim_num) - prefix;
+        if (prefix[r_idx] - prefix[i - 1] == n) {
+            cout << i << " " << r_idx << '\n';
         }
     }
     return 0;
