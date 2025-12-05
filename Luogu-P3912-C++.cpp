1b909d05067416e117c883dc43a149d3fe3d9615
nine19een <nine19eenowo@gmail.com>
2025-12-05 22:25:35 +0800

    Creat P3912

---
+++ Luogu-P3912-C++.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int MAXN = 100000005, MAXP = 6000000;
+
+int cnt, n;
+bool not_prime[MAXN];
+vector<int> primes;
+
+int main(){
+	primes.reserve(MAXP);
+	cin >> n;
+	for(int i = 2; i <= n; ++i){
+		if(!not_prime[i]){
+			primes.push_back(i);
+			cnt++;
+		}
+		for(int p: primes){
+			if(p * i > n){
+				break;
+			}
+			not_prime[p * i] = true;
+			if(i % p == 0){
+				break;
+			}
+		}
+	}
+	cout << cnt;
+	return 0;
+}
