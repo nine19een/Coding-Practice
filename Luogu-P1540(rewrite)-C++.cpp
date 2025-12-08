d3b32deb92cfdb34ac451a77e217befb19ef1fa9
nine19een <nine19eenowo@gmail.com>
2025-12-08 20:47:07 +0800

    creat P1540rewrite

---
+++ Luogu-P1540(rewrite)-C++.cpp
@@ -0,0 +1,32 @@
+// Problem:  Luogu P1540 [NOIP 2010 提高组] 机器翻译
+// Link:     https://www.luogu.com.cn/problem/P1540
+// Author:   nine19een
+// Date:     2025-12-08
+
+#include<bits/stdc++.h>
+using namespace std;
+
+queue<int> q;
+unordered_set<int> s;
+int m, n, cnt;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> m >> n;
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        if (!s.count(num)) {
+            cnt++;
+            q.push(num);
+            s.insert(num);
+            if (s.size() > m) {
+                s.erase(q.front());
+                q.pop();
+            }
+        }
+    }
+    cout << cnt;
+    return 0;
+}
